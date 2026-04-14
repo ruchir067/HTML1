e54bbadce91dfca092a04fa4f40ffe9171c8e0d5
Ruchir Ram Mishra <mishraruchir07@gmail.com>
2026-04-14 07:33:29 +0000

    day 7

--- latest/src/App.jsx
+++ latest/src/App.jsx
@@ -4,33 +4,17 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 function App(){
-
-function fun(){
-   alert("you click on button")
-}
-const fruit=(name,AP,BN)=>{
-    alert(name,AP,BN)
-}
-let data ="Demo"
-function updata(){
-   data = "Example"
-   alert(data);
-}
-
-
-   return(
-   <div>
-
-<h1> example of arrow function</h1>
-<button onClick={fun}> Click to call function </button>
-<button onClick={()=>fruit("first Parameter")}> arrow fruit button </button>
-<button onClick={()=>fruit("you click on apple")}> Apple </button>
-<button onClick={()=>fruit("you click on banana")}> Banana </button>
-<h2> {data} </h2>
-<button onClick={updata}> click to change value </button>
-</div>
+   const[data,setdata]=useState("hello");
+   function fun(){
+      setdata("Changed Value")
+   }
+   return (
+      <>
+      <h1>State of variable</h1>
+      <h2>{data}</h2>
+      <button onClick={fun}>click</button>
+      
+      </>
    )
-
   }
-
 export default App
